54a783a58fb5119517693a08f38fd1f2157a4841
Jose Castro <josehco85@gmail.com>
2026-05-23 19:02:02 -0400

    first

---
+++ rrhh_nest/src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { HelloResolver } from './hello.resolver';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      playground: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService, HelloResolver],
+})
+export class AppModule {}
---
+++ rrhh_spring_boot/src/main/java/com/rrhh/core/RrhhSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.rrhh.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RrhhSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RrhhSpringBootApplication.class, args);
+	}
+
+}
